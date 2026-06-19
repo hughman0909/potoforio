e206ecab55d92723b6f6f162bac9a9e25280ee1d
Hinata Yamaguchi <meru7374@gmail.com>
2026-06-20 04:43:28 +0900

    abcde

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://hughman0909.github.io', 
+	site: 'https://hughman0909.github.io',
+	base: 'portfolio',
 });
 
